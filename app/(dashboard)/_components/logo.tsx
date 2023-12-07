12c67c54fa3fcdd9fe26135e2d839d9bfc124cab
unknown <ap7171363@gmail.com>
2023-12-08 00:25:08 +0530

    logo and some minor changes

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 export const Logo = () => {
   return (
-    <Image
-      height={130}
-      width={130}
-      alt="logo"
-      src="/logo.svg"
-    />
+    <div className='flex flex-row text-center justify-center items-center'>
+            <Image 
+        height={60}
+        width={60}
+        alt='logo'
+        src='/image/logo.png'
+        />
+        <h2 className= " ml-2 font-bold ">सृष्टि IAS</h2>
+    </div>
   )
 }
