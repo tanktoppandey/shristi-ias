db605f56cb24b89a3cc8cdd36a2c3ff6882b7e7c
unknown <ap7171363@gmail.com>
2023-12-08 00:28:18 +0530

    minor change

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -7,7 +7,7 @@ export const Logo = () => {
         height={60}
         width={60}
         alt='logo'
-        src='/image/logo.png'
+        src='/images/logo.png'
         />
         <h2 className= " ml-2 font-bold ">सृष्टि IAS</h2>
     </div>
